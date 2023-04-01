a7991f604002bb3ef918c3341d21cbdbdc772458
Rachel Prather <rachelprather1@gmail.com>
2023-04-01 13:26:33 -0700

    Change alt text for mini-cover display

--- src/main.js
+++ src/main.js
@@ -166,7 +166,7 @@ function displaySavedCovers() {
     savedCoverSection.innerHTML += 
     `
     <section class="mini-cover">
-      <img class="cover-image" id="${savedCovers[i].id}" src="${savedCovers[i].coverImg}" alt="RomCom image should be displayed">
+      <img class="cover-image" id="${savedCovers[i].id}" src="${savedCovers[i].coverImg}" alt="miniature RomCom cover">
       <h2 class="cover-title">${savedCovers[i].title}</h2>
       <h3 class="tagline">A tale of <span class="tagline-1">${savedCovers[i].tagline1}</span> and <span class="tagline-2">${savedCovers[i].tagline2}</span></h3>
       <img class="price-tag" src="./assets/price.png">
