9a3d24ca97e8144ff067895d3feb555831cee8bd
Rachel Prather <rachelprather1@gmail.com>
2023-04-01 12:14:38 -0700

    Rearrange variables to all be in same section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 // Create variables targetting the relevant DOM elements here 👇
+// We've provided a few variables below
+// var savedCovers = [
+//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+
+var currentCover;
 
   // Cover elements:
 var coverImage = document.querySelector(".cover-image");
@@ -45,21 +51,6 @@ saveCoverButton.addEventListener("click", saveCover);
 savedCoverSection.addEventListener("dblclick", deleteCover);
 
 // Create your event handlers and other functions here 👇
-
-// We've provided a few variables below
-// var savedCovers = [
-//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
-// ];
-
-
-// Variables
-
-// var savedCovers = [
-//   createCover(covers[getRandomIndex(cover)], titles[getRandomIndex(title)], 
-//   descriptors[getRandomIndex(descriptors)], 
-//   descriptors[getRandomIndex(descriptors)])
-// ];
-var currentCover;
 
 // We've provided two functions to get you started 
 
