015b89bd4632c4d35d91137e77340aae573eded9
Parvin Sattorova <nanajon66@gmail.com>
2023-04-01 19:32:27 -0600

    refak: add fixs and refactoring createNewCover function

--- src/main.js
+++ src/main.js
@@ -1,56 +1,61 @@
-// Create variables targeting the relevant DOM elements here 👇
+// Create variables targetting the relevant DOM elements here 👇
 
-// Sample cover:
-  // createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// querySelector variables.
 
-var savedCovers = [];
-var currentCover;
-
-  // Cover elements:
+// Cover elements.
 var coverImage = document.querySelector(".cover-image");
 var coverTitle = document.querySelector(".cover-title");
 var tagline1 = document.querySelector(".tagline-1");
 var tagline2 = document.querySelector(".tagline-2");
 
-  // Top Row Buttons:
+// Buttons:
 var homeButton = document.querySelector(".home-button");
 var randomCoverButton = document.querySelector(".random-cover-button");
 var saveCoverButton = document.querySelector(".save-cover-button");
 var viewSavedCoversButton = document.querySelector(".view-saved-button");
-var makeYourOwnCoverButton = document.querySelector(".make-new-button");
+var makeNewCoverButton = document.querySelector(".make-new-button");
+var makeMyBookButton = document.querySelector('.create-new-book-button');
 
-  // Views:
+// View elements.
 var homeView = document.querySelector(".home-view");
 var formView = document.querySelector(".form-view");
 var savedView = document.querySelector(".saved-view");
 var savedCoverSection = document.querySelector(".saved-covers-section");
 
-  // Form elements:
+// Form elements.
 var userInputCover = document.querySelector(".user-cover");
 var userInputTitle = document.querySelector(".user-title");
 var userInputDesc1 = document.querySelector(".user-desc1");
 var userInputDesc2 = document.querySelector(".user-desc2");
-var makeMyBookButton = document.querySelector(".create-new-book-button");
+var createNewBookButton = document.querySelector(".create-new-book-button");
 
 // Add your event listeners here 👇
-
-  // Generate random covers:
 window.addEventListener("load", getRandomCover);
 randomCoverButton.addEventListener("click", getRandomCover);
-
-  // Change views:
-makeYourOwnCoverButton.addEventListener("click", viewMakeYourOwnCover);
+makeNewCoverButton.addEventListener("click", viewMakeYourOwnCover);
 viewSavedCoversButton.addEventListener("click", viewSavedCovers);
 homeButton.addEventListener("click", viewHomePage);
-
-  // Make your own cover:
-makeMyBookButton.addEventListener("click", makeMyBook);
-
-  // Save & Delete covers:
+createNewBookButton.addEventListener("click", createNewCover);
 saveCoverButton.addEventListener("click", saveCover);
 savedCoverSection.addEventListener("dblclick", deleteCover);
 
+
 // Create your event handlers and other functions here 👇
+
+// We've provided a few variables below
+// var savedCovers = [
+//   createCover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
+// ];
+
+
+// Variables
+
+var savedCovers = [
+  createCover(covers[getRandomIndex(cover)], titles[getRandomIndex(title)], 
+  descriptors[getRandomIndex(descriptors)], 
+  descriptors[getRandomIndex(descriptors)])
+];
+var currentCover;
 
 // We've provided two functions to get you started 
 
@@ -86,11 +91,22 @@ function viewMakeYourOwnCover() {
   saveCoverButton.classList.add('hidden');
   randomCoverButton.classList.add('hidden');
   homeButton.classList.remove('hidden');
-  makeYourOwnCoverButton.classList.remove('hidden');
+  makeNewCoverButton.classList.remove('hidden');
   viewSavedCoversButton.classList.remove('hidden');
+  makeNewCoverButton.classList.remove('hidden');
 };
 
-
+function viewSavedCovers() {
+  homeView.classList.add('hidden');
+  formView.classList.add('hidden');
+  homeButton.classList.remove('hidden');
+  savedView.classList.remove('hidden');
+  makeNewCoverButton.classList.remove('hidden');
+  randomCoverButton.classList.add('hidden');
+  saveCoverButton.classList.add('hidden');
+  savedCoverSection.innerHTML = '';
+  showSavedCovers();
+};
 
 function viewHomePage() {
   formView.classList.add('hidden');
@@ -100,79 +116,12 @@ function viewHomePage() {
   randomCoverButton.classList.remove('hidden');
   saveCoverButton.classList.remove('hidden');
   viewSavedCoversButton.classList.remove('hidden');
-  makeYourOwnCoverButton.classList.remove('hidden');
+  makeNewCoverButton.classList.remove('hidden');
   savedCoverSection.innerHTML = ``;
 };
 
-function makeMyBook(event) {
-  event.preventDefault();
-
-  if(!userInputCover.value || !userInputTitle.value || !userInputDesc1.value || !userInputDesc2.value) {
-  alert("Error “Please fill out all required fields. Thank you!");
-  } else {
-    currentCover = createCover(userInputCover.value, userInputTitle.value, userInputDesc1.value, userInputDesc2.value)
-
-    coverImage.src = currentCover.coverImg
-    coverTitle.innerText = currentCover.title
-    tagline1.innerText = currentCover.tagline1
-    tagline2.innerText = currentCover.tagline2
-
-    pushValuesToArray();
-
-    viewHomePage();
-
-    document.querySelector("form").reset();
-  };
-};
-
-function pushValuesToArray() {
-  covers.push(userInputCover.value);
-  titles.push(userInputTitle.value);
-  descriptors.push(userInputDesc1.value);
-  descriptors.push(userInputDesc2.value);
-};
-
-
-function viewMakeYourOwnCover() {
-  homeView.classList.add('hidden');
-  savedView.classList.add('hidden');
-
-  formView.classList.remove('hidden');
-
-  saveCoverButton.classList.add('hidden');
-  randomCoverButton.classList.add('hidden');
-
-  homeButton.classList.remove('hidden');
-  makeYourOwnCoverButton.classList.remove('hidden');
-  viewSavedCoversButton.classList.remove('hidden');
-};
-
-function saveCover() {
-  if (!savedCovers.includes(currentCover)) {
-    savedCovers.push(currentCover);
-    displaySavedCovers(); 
-  } else {
-    alert("This RomCom Cover has already been saved!");
-  }; 
-}
-
-function viewSavedCovers() {
-  homeView.classList.add('hidden');
-  formView.classList.add('hidden');
-
-  homeButton.classList.remove('hidden');
-  savedView.classList.remove('hidden');
-  makeYourOwnCoverButton.classList.remove('hidden');
-
-  randomCoverButton.classList.add('hidden');
-  saveCoverButton.classList.add('hidden');
-
-  savedCoverSection.innerHTML = '';
-  displaySavedCovers();
-};
-
-function displaySavedCovers() {
-  for (var i = 0; i < savedCovers.length; i++) {
+function showSavedCovers() {
+  for(var i = 0; i < savedCovers.length; i++) {
     savedCoverSection.innerHTML += 
     `
     <section class="mini-cover">
@@ -182,7 +131,7 @@ function displaySavedCovers() {
       <img class="price-tag" src="./assets/price.png">
       <img class="overlay" src="./assets/overlay.png">
     </section>
-    `
+      `
   };
 };
 
@@ -195,10 +144,24 @@ function saveCover(){
   };
 };
 
-function createNewCover(event) {
+  // //updating data model:
+  // var inputCover = userCover.value;
+  // var inputTitle = userTitle.value;
+  // var inputDesc1 = userDescriptor1.value;
+  // var inputDesc2 = userDescriptor2.value;
+
+  // covers.push(inputCover);
+  // titles.push(inputTitle);
+  // descriptors.push(inputDesc1, inputDesc2);
+
+  // currentCover = new Cover(inputCover, inputTitle, inputDesc1, inputDesc2);
+
+  // // updating DOM:
+  // displayCover();
+  // displayHomePage();
+
+function createNewCover() {
   event.preventDefault();
-  console.log(createNewCover)
-  console.log(event)
   if(!userInputCover.value || !userInputTitle.value || !userInputDesc1.value || !userInputDesc2.value) {
   alert("Error “Please fill out all required fields. Thank you!");
   }else {
